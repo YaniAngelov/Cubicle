dabe5261fe4eadf4e8a1958ba1f47e4860e61d96
Yani Angelov <56196118+YaniAngelov@users.noreply.github.com>
2021-04-01 09:35:38 +0300

    Search and difficulty level fixed

--- services/productService.js
+++ services/productService.js
@@ -3,9 +3,25 @@ const Cube = require('../models/Cube');
 const fs = require('fs/promises');
 const path = require('path');
 const productsData = require('../database/products.json');
+const { search } = require('../controllers/productController');
 
-function getAll() {
-    return productsData;
+function getAll(query) {
+    let result = productsData;
+
+    if (query.search) {
+        result = result.filter(
+            x => x.name.toLocaleLowerCase().includes(query.search.toLocaleLowerCase()));
+    }
+
+    if (query.from) {
+        result = result.filter(x => Number(x.level) >= query.from);
+    }
+
+    if (query.to) {
+        result = result.filter(x => Number(x.level) <= query.to);
+    }
+
+    return result;
 }
 
 function create(data, callback) {
